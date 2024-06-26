efa49671d82abb580bed8250059dd291c602573d
NicPH <admslavmkl@gmail.com>
2024-06-26 09:00:00 +0300

    s04 t 10

--- sprint_04/sprint_04.js
+++ sprint_04/sprint_04.js
@@ -92,7 +92,7 @@ function t08 () {
 document.querySelector('.b-8').onclick = t08;
 
 
-// Task 9 ---------
+// Task 9
 // Запустите функцию t09 и посмотрите на результат.
 
 function t09 () {
@@ -102,17 +102,13 @@ function t09 () {
 document.querySelector('.b-9').onclick = t09;
 
 
-// Task 10 ---------
+// Task 10
 // Давайте научимся отключать события заданные через свойство элемента (on). Изучите как работает функция t10. Нажмите кнопку несколько раз. Затем, удалите комментарий со строки внутри функции. Нажмите кнопку несколько раз.
 
 function t10 () {
     document.querySelector('.out-10').innerHTML += 'function 10 work<br>';
-    // document.querySelector('.b-10').onclick = null;
+    document.querySelector('.b-10').onclick = null;
  }
  
  document.querySelector('.b-10').onclick = t10;
 
-// ************************************
-
-// Task 9
-// Task 10
