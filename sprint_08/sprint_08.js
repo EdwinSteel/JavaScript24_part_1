025a3a6d5d6b72375b4da636bc964e766797a24e
NicPH <admslavmkl@gmail.com>
2024-08-13 09:04:46 +0300

    s08

--- sprint_08/sprint_08.js
+++ sprint_08/sprint_08.js
@@ -225,3 +225,25 @@ function t20() {
 }
 
 document.querySelector('.b-20').onclick= t20;
+
+// *****************************************************************
+// Task 1
+// Task 2
+// Task 3
+// Task 4
+// Task 5
+// Task 6
+// Task 7
+// Task 8
+// Task 9
+// Task 10
+// Task 11
+// Task 12
+// Task 13
+// Task 14
+// Тask 15
+// Task 16
+// Task 17
+// Task 18
+// Task 19
+// Task 20
