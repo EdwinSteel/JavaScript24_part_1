221ccae369a4e19a29e33c1349e686f137fc6df0
NicPH <admslavmkl@gmail.com>
2024-11-25 16:12:50 +0200

    s11 t01

--- sprint_11/sprint_11.js
+++ sprint_11/sprint_11.js
@@ -94,3 +94,15 @@ function t10 () {
 }
 
 document.querySelector('.b-10').onclick = t10;
+
+// ************************************
+// Task 1
+// Task 2
+// Task 3
+// Task 4
+// Task 5
+// Task 6
+// Task 7
+// Task 8
+// Task 9
+// Task 10
